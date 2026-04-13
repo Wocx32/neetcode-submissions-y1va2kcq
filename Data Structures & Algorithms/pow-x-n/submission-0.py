094061a7a396215e71b2196b476d55964a203505
Wocx <63423836+Wocx32@users.noreply.github.com>
2026-04-13 00:16:39 -0400

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        
+        if n == 0:
+            return 1
+        
+        res = 1
+        power = abs(n)
+
+        while power > 0:
+            if power % 2 != 0:
+                res *= x
+            
+            x = x*x
+            power = power >> 1
+        
+        return res if n >= 0 else 1/res
