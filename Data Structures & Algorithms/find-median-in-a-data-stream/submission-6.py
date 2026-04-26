bd072570ad9011b560b95b4021e5ee664a5523ad
Wocx <63423836+Wocx32@users.noreply.github.com>
2026-04-25 21:45:40 -0400

    Update: find-median-in-a-data-stream - submission-6

--- Data Structures & Algorithms/find-median-in-a-data-stream/submission-6.py
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-6.py
@@ -1,29 +1,32 @@
 class MedianFinder:
 
     def __init__(self):
-        self.min_heap = []
-        self.max_heap = []
+        self.left_max = []
+        self.right_min = []
 
     def addNum(self, num: int) -> None:
-
-        if self.max_heap and num < self.max_heap[0]:    
-            heapq.heappush_max(self.max_heap, num)
-        else:
-            heapq.heappush(self.min_heap, num)
-
-        if len(self.min_heap) - len(self.max_heap) > 1:
-            item = heapq.heappop(self.min_heap)
-            heapq.heappush_max(self.max_heap, item)
-        elif len(self.max_heap) - len(self.min_heap) > 1:
-            item = heapq.heappop_max(self.max_heap)
-            heapq.heappush(self.min_heap, item)
         
+        if self.right_min and num < self.right_min[0]:
+            heapq.heappush_max(self.left_max, num)
+        else:
+            heapq.heappush(self.right_min, num)
+        
+        if len(self.left_max) - len(self.right_min) > 1:
+            val = heapq.heappop_max(self.left_max)
+            heapq.heappush(self.right_min, val)
+        
+        if len(self.right_min) - len(self.left_max) > 1:
+            val = heapq.heappop(self.right_min)
+            heapq.heappush_max(self.left_max, val)
+
 
     def findMedian(self) -> float:
         
-        if (len(self.min_heap) + len(self.max_heap)) % 2 == 0:
-            return (self.min_heap[0] + self.max_heap[0]) / 2
-        elif len(self.min_heap) > len(self.max_heap):
-            return self.min_heap[0]
+        if len(self.left_max) == len(self.right_min):
+            return (self.left_max[0] + self.right_min[0]) / 2
+        
+        elif len(self.left_max) > len(self.right_min):
+            return self.left_max[0]
+        
         else:
-            return self.max_heap[0]
+            return self.right_min[0]
