b6b881d61e657e3583e55ab9413966fc8e0530b9
Wocx <63423836+Wocx32@users.noreply.github.com>
2026-06-02 03:00:09 -0400

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        dp = {}
+
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+
+            if i in dp:
+                return dp[i]
+
+            dp[i] = max(dfs(i + 1), dfs(i + 2) + nums[i])
+            return dp[i]
+
+        return dfs(0)
